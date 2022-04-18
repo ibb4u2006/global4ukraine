3e8413337059fa5fbae1f568ac9dbf9efbf40652
Ibrahim Bello <ibb@Ibrahims-MacBook-Pro.local>
2022-04-18 14:38:39 +0200

    Global meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,19 +5,49 @@ import Layout from "../component/layout";
 import GlobalStyle from "../styles/gobal";
 import SimpleReactLightbox from "simple-react-lightbox";
 import { appWithTranslation } from "next-i18next";
+import Head from "next/head";
+import { useTranslation } from "next-i18next";
+import { getServerSideTranslations } from "../utils/translations";
+
+export async function getStaticProps({ locale }: any) {
+  const translation = await getServerSideTranslations(locale, [
+    "common",
+    "home",
+  ]);
+  return {
+    props: { ...translation },
+  };
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { t } = useTranslation();
   return (
-    <ThemeProvider theme={Theme}>
-      <ChakraProvider theme={Theme}>
-        <GlobalStyle />
-        <Layout>
-          <SimpleReactLightbox>
-            <Component {...pageProps} />
-          </SimpleReactLightbox>
-        </Layout>
-      </ChakraProvider>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>{t("common:hero-title")}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          property="og:title"
+          content={t("common:hero-title")}
+          key={t("common:hero-title")}
+        />
+        <meta
+          property="og:description"
+          content={t("common:meta-description")}
+          key={t("common:meta-description")}
+        />
+      </Head>
+      <ThemeProvider theme={Theme}>
+        <ChakraProvider theme={Theme}>
+          <GlobalStyle />
+          <Layout>
+            <SimpleReactLightbox>
+              <Component {...pageProps} />
+            </SimpleReactLightbox>
+          </Layout>
+        </ChakraProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
