806d6ff9f69ae61745501816881330996351fdb5
Ibrahim Bello <ibb@Ibrahims-MacBook-Pro.local>
2022-04-18 13:57:10 +0200

    Added new partner

--- data/partners.ts
+++ data/partners.ts
@@ -1,8 +1,13 @@
 export const PARTNERS = [
-  { id: 0, img: "/img/falkon.png", url: "https://www.falkon-ua.org/" },
+  { id: 0, img: "/img/partners/falkon.png", url: "https://www.falkon-ua.org/" },
   {
     id: 1,
-    img: "/img/praha-nebusice.png",
+    img: "/img/partners/praha-nebusice.png",
     url: "https://www.prahanebusice.cz/",
+  },
+  {
+    id: 2,
+    img: "/img/partners/lego.svg",
+    url: "https://www.lego.com/en-cz",
   },
 ];
